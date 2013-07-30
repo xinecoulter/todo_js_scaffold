e798a1a1f55e6454a3cc76e6d7caff24bbe386b5
Christine Coulter <christinetcoulter@gmail.com>
2013-07-29 23:52:52 -0400

    Complete step 3

--- src/todo.js
+++ src/todo.js
@@ -1,10 +1,10 @@
 var todoApp = {
-  createTask: function(input){
+  createTask: function(input) {
     var newTodoItem = Object.create(todoItem);
     newTodoItem.setTaskText(input);
     this.appendTask(newTodoItem);
   },
-  appendTask: function(newTodoItem){
+  appendTask: function(newTodoItem) {
     if (newTodoItem.taskName !== "") {
       newTodoItem.render();
     }
@@ -13,18 +13,24 @@ var todoApp = {
 
 var todoItem = {
   taskName: "",
-  setTaskText: function(input){
+  setTaskText: function(input) {
     this.taskName += input;
   },
-  render: function(){
+  render: function() {
     var liTag = document.createElement("li");
-    liTag.innerText = this.taskName;
+    var taskText = document.createElement("div");
+    taskText.innerHTML = this.taskName;
+    taskText.className = "task-text";
+    var taskButtons = document.createElement("div");
+    taskButtons.className = "task-buttons";
     document.getElementById("todo-items").appendChild(liTag);
+    liTag.appendChild(taskText);
+    taskText.appendChild(taskButtons);
   }
 };
 
 if (typeof Object.create !== 'function') {
-  Object.create = function (o) {
+  Object.create = function(o) {
     function F() {}
     F.prototype = o;
     return new F();
